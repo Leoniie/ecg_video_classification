2d2d18048de4a29dff3c48ed1b5770f64ce63d76
Sebastian Windeck <sebastian.windeck@web.de>
2018-11-20 17:57:35 +0100

    Add TODOs to helper files

---
+++ helpers/io.py
@@ -0,0 +1,25 @@
+def Inputter():
+
+    # pass is a placeholder for the real function
+    pass
+
+    # TODO: define input arguments
+
+    # TODO: reformat the input to numpy array
+
+    # TODO: return numpy array
+
+
+def Outputter():
+
+    # pass is a placeholder for the real function
+    pass
+
+    # TODO: define the arguments
+
+    # TODO: name of the output file is 'daytime_solution.csv'
+
+    # TODO: generate output file in folder './output'
+
+    # no return required
+
