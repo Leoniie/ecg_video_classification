d61b6899ec49b553ab606f9a2627b8efcaa80548
Sebastian Windeck <sebastian.windeck@web.de>
2018-11-21 01:21:50 +0100

    Adding TODOs for modelling

---
+++ models/Functional.py
@@ -0,0 +1,10 @@
+# TODO: Create a simple sequential Conv2DLSTM NN
+
+
+def build_functional():
+    pass
+    # TODO: build function
+
+def compile_functional():
+    pass
+    # TODO: compile
---
+++ models/Sequential.py
@@ -0,0 +1,10 @@
+# TODO: Create a simple sequential Conv2DLSTM NN
+
+
+def build_sequential():
+    pass
+    # TODO: build function
+
+def compile_sequential():
+    pass
+    # TODO: compile
