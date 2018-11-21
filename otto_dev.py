2ddf21ab4dd18b207dbc21a54905170c9cfefcbc
Sebastian Windeck <sebastian.windeck@web.de>
2018-11-21 01:38:04 +0100

    Change position of playground files

--- otto_dev.py
+++ otto_dev.py
@@ -32,3 +32,18 @@ if not os.path.exists(tf_record_test):
 	save_tf_record(x_test,tf_record_test)
 
 # Model
+
+
+## Example from
+feature_col_video = tf.feature_column.numeric_column('video',shape=[212,100,100],dtype=tf.uint8)
+feature_col_frame = tf.feature_column.numeric_column('frame',shape = [100,100],dtype=tf.uint8)
+batchsize_video = 1
+
+estimator = tf.estimator.LinearClassifier(feature_columns = [feature_col_video],model_dir='tf_checkpoints')
+estimator.train(input_fn=lambda: input_fn_from_dataset(tf_record_train, batch_size=batchsize_video),max_steps=1)
+pred = estimator.predict(input_fn=lambda: input_fn_from_dataset(tf_record_test, batch_size=batchsize_video,num_epochs=1,shuffle = False))
+y_sol = prob_positive_class_from_prediction(pred)
+
+# Generate output
+
+#outputter(y_sol)
--- playground/otto_dev.py
+++
@@ -1,3 +0,0 @@
-from helpers.io import inputter_csv
-
-df_y = inputter_csv('..\\data\\train_target.csv')
