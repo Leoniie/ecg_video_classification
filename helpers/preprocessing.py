de9db1e8d9b82ed5432ad6917f108f7a1175fe15
Sebastian Windeck <sebastian.windeck@web.de>
2018-11-21 01:35:55 +0100

    Adding Preprocessing TODOs

---
+++ helpers/preprocessing.py
@@ -0,0 +1,24 @@
+# TODO: Create preprocessing pipeline with plugable substeps
+# possible library to use 'skvideo.utils'
+
+def scaling():
+    pass
+    # TODO: use scikit video or cv2 to rescale the image/video
+
+def cropping():
+    pass
+    # TODO: reduce the image to moving pixels
+
+def normalize():
+    pass
+    # TODO: add normalizer
+    # possibility to reduce values from 0 to 255
+
+def preprocessing():
+    pass
+    # TODO: add the relevant arguments to the function
+    # TODO: add binary decision variables
+    # TODO: add the substeps to the function
+    # TODO: return the preprocessed data set
+    # TODO: add Exceptions
+    # be aware, no hard coding to be adaptable for train and test data
