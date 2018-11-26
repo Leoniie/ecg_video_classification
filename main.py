dfcd472129ba1005b7c45782727fd4017d903cb9
Sebastian Windeck <sebastian.windeck@web.de>
2018-11-26 10:08:04 +0100

    Update main.py
    Typo fixed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # Input folders
 
 train_folder = os.path.join(dir_path,"data\\train")
-test_folder = os.path.join(dir_path," gdata\\test")
+test_folder = os.path.join(dir_path,"data\\test")
 
 # Create tf records in super folder for train records outside git-repository
 
