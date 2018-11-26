9c9d7b981923ca58621c940686d7f208e1d250e5
Malte993 <36504976+Malte993@users.noreply.github.com>
2018-11-26 10:06:14 +0100

    Update Sequential.py

--- models/Sequential.py
+++ models/Sequential.py
@@ -1,8 +1,16 @@
 # TODO: Create a simple sequential Conv2DLSTM NN
+#Malte
 
 
 def build_sequential():
-    pass
+    model = Sequential()
+    # define CNN model
+    model.add(TimeDistributed(Conv2D(...))
+    model.add(TimeDistributed(MaxPooling2D(...)))
+    model.add(TimeDistributed(Flatten()))
+    # define LSTM model
+    model.add(LSTM(...))
+    model.add(Dense(...))
     # TODO: build function
 
 def compile_sequential():
