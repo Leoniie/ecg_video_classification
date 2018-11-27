23b74b76846ba0b2405159f59a1416691135e7c3
LeoMug <leoniem@student.ethz.ch>
2018-11-27 19:39:02 +0100

    preprocessing: give same max_time to x_train and x_test

--- helpers/preprocessing.py
+++ helpers/preprocessing.py
@@ -2,7 +2,7 @@ import numpy as np
 from skimage.transform import rescale, resize, downscale_local_mean
 
 
-def scaling(df, resolution_type = 'resize', resolution = 0.5):
+def scale(df, resolution_type='resize', resolution=0.5):
     """
     :param df: 5d-array [sample, steps, height, width, channels]
     :param resolution_type: resize, rescale, downsize
@@ -12,103 +12,64 @@ def scaling(df, resolution_type = 'resize', resolution = 0.5):
     # change resolution
     if resolution_type == 'resize':
         df = resize(df, (df.shape[0], df.shape[1], int(df.shape[2] * resolution),
-                         int(df.shape[3] * resolution), df.shape[1]),
+                         int(df.shape[3] * resolution), df.shape[4]),
                     mode='constant')
     elif resolution_type == 'rescale':
         df = rescale(df, (df.shape[0], df.shape[1], int(df.shape[2] * resolution),
-                         int(df.shape[3] * resolution), df.shape[1]),
+                          int(df.shape[3] * resolution), df.shape[4]),
                      mode='constant')
     elif resolution_type == 'downscale':
         df = downscale_local_mean(df, (df.shape[0], df.shape[1], int(df.shape[2] * resolution),
-                         int(df.shape[3] * resolution), df.shape[1]),
-                                mode='constant')
+                                       int(df.shape[3] * resolution), df.shape[4]))
     else:
         print('Wrong resolution_type detected')
 
     return df
+
 
 # leonie
 def cropping():
     pass
     # TODO: reduce the image to moving pixels
 
-def normalize():
-    pass
-    # done in preprocessing anyway
 
-    # possibility to reduce values from 0 to 255
+def normalize(df):
+    df_max_frame = np.max(abs(df), 0)
+    df = df / df_max_frame
+    return df
 
-def preprocessing(x,max_time):
-    #input: list of 3d arrays with shape (timesteps, height, width)
-    #also includes normalization
 
-    x_array = np.zeros((x.shape[0], max_time, x[0].shape[1], x[0].shape[2]))
+def list_to_array(x_data, maxtime):
+    x_array = np.zeros((x_data.shape[0], maxtime, x_data[0].shape[1], x_data[0].shape[2]))
 
-    for i in np.arange(x.shape[0]):
-        v = x[i]
-        d = np.max(abs(v))
-        np.swapaxes(v, 1, 2)
-        np.swapaxes(v, 0, 1)
-        v = v / d
+    for i in np.arange(x_data.shape[0]):
+        v = x_data[i]
         x_array[i, :v.shape[0], :v.shape[1], :v.shape[2]] = v
 
+    # swap time axis from 3rd to 2nd dimension
+    np.swapaxes(x_array, 2, 3)
+    np.swapaxes(x_array, 2, 1)
+
     x_array = np.resize(x_array, (x_array.shape[0], x_array.shape[1], x_array.shape[2], x_array.shape[3], 1))
 
     return x_array
+
 
 def max_time(x):
-    
-    max_time = 0
+    maxtime = 0
     for i in np.arange(x.shape[0]):
-        max_time = np.max((max_time, (x[i]).shape[0]))
+        maxtime = np.max((maxtime, (x[i]).shape[0]))
 
-    return max_time
-
-
-
-def preprocessing_scaled(x,max_time):
-    #input: list of 3d arrays with shape (timesteps, height, width)
-    #also includes normalization
+    return maxtime
 
 
-    x_array = np.zeros((x.shape[0], max_time, 50, 50))
+def preprocessing(x_data, max_time, normalizing=True, scaling=True, resolution_type='resize', resolution=0.5):
+    df = list_to_array(x_data, max_time)
+    if normalizing:
+        df = normalize(df)
+    if scaling:
+        df = scale(df, resolution_type, resolution)
 
+    return df
 
-def preprocessing_scaled(x, max_time):
-    # input: list of 3d arrays with shape (timesteps, height, width)
-    # also includes normalization
-
-    x_array = np.zeros((x.shape[0], max_time, 50, 50))
-
-    for i in np.arange(x.shape[0]):
-        v = x[i]
-        v_scaled = np.zeros((v.shape[0], 50, 50))
-        for j in np.arange(v_scaled.shape[0]):
-            a = np.zeros((50, 50))
-            image = v[j, :, :]
-            for l in np.arange(50):
-                for k in np.arange(50):
-                    a[l, k] = np.sum(image[2 * l:2 * l + 2, 2 * k:2 * k + 2])
-            v_scaled[j, :, :] = a
-        d = np.max(abs(v_scaled))
-        v_scaled = v_scaled/d
-        np.swapaxes(v,1,2)
-        np.swapaxes(v,0,1)
-        x_array[i,:v_scaled.shape[0], :v_scaled.shape[1], :v_scaled.shape[2]]= v_scaled
-
-
-    x_array = np.resize(x_array, (x_array.shape[0], x_array.shape[1], x_array.shape[2], x_array.shape[3], 1))
-
-    return x_array
-
-
-
-
-    #add the relevant arguments to the function
-    # TODO: add binary decision variables
-    # TODO: add the substeps to the function
-    # TODO: return the preprocessed data set
     # TODO: add Exceptions
-
-
-# be aware, no hard coding to be adaptable for train and test data
